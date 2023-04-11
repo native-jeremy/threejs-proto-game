c32a7f106ed7a524a7c8491401393833ea202899
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-04-11 16:49:29 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer( { antialiasing: true } );
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-const geometry = new THREE.BoxGeometry( 3, 2, 2 );
+const geometry = new THREE.BoxGeometry( 4, 4, 4 );
 //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const texture = new THREE.TextureLoader().load('images/texture3.gif');
 const material = new THREE.MeshBasicMaterial( { map: texture } );
@@ -29,8 +29,8 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.00;
-	cube.rotation.y += 0.02;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
