49e498b59db0b349c5672fb610864a67dde0dbf5
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-04-12 09:57:35 +1000

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+const app = document.querySelector('#app');
+const colorParam = '#fffff';
+
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(colorParam.color);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer( { antialiasing: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+app.append( renderer.domElement );
 
 
-const geometry = new THREE.BoxGeometry( 3, 2, 2 );
+const geometry = new THREE.BoxGeometry( 3, 1.5, 3 );
 //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const texture = new THREE.TextureLoader().load('images/texture3.gif');
+const texture = new THREE.TextureLoader().load('images/native.jpg');
 const material = new THREE.MeshBasicMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
