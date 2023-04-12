cfb511aac02bb52b17f3461e949872f1477cd8fe
MorganBrownDev <mjxbrown@gmail.com>
2023-04-13 02:54:39 +1000

    Update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,71 @@
+import * as THREE from 'three';
+
 const app = document.querySelector('#app');
-const colorParam = '#fffff';
+let camera, scene, renderer; // Three JS Globals
+const originalBoxSize = 3; // Original width and height of a box
+const colorParam = '#000000';
 
+init();
+
+function init() {
+
+// Scene 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(colorParam.color);
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-app.append( renderer.domElement );
-
-
-const geometry = new THREE.BoxGeometry( 3, 2, 2 );
-//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//scene.background = new THREE.Color(colorParam.color);
+const geometry = new THREE.BoxGeometry( 4, 2, 2 );
 const texture = new THREE.TextureLoader().load('images/native.jpg');
-texture.magFilter = THREE.NearestFilter;
-const material = new THREE.MeshBasicMaterial( { map: texture } );
+const material = new THREE.MeshLambertMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
+cube.position.set(0, 0, 0);
 scene.add( cube );
 
+
+// Foundation
+//addLayer(0.8, originalBoxSize, originalBoxSize);
+
+// First layer
+//addLayer(-18, 0, originalBoxSize, originalBoxSize, "x");
+
+// Lights
+const ambientlight = new THREE.AmbientLight( 0x504040, 0.2 ); // soft white light
+scene.add( ambientlight );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.2 );
+scene.add( directionalLight );
+
+
+// Camera
+const angle = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(
+	 20, 
+	 angle, 
+	 1, 
+	 100 );
+
+camera.position.z = 5;
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer( { antialias: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.render( scene, camera );
+app.append( renderer.domElement );
+
+// Animate function
+function animate() {
+	requestAnimationFrame( animate );
+	
+	cube.rotation.x += 0.0025;
+	cube.rotation.y += 0.00;
+
+	renderer.render( scene, camera );
+}
+
+animate();
+
+};
+
+// Resizing the scene
 window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize(){
@@ -28,16 +76,3 @@ function onWindowResize(){
     renderer.setSize( window.innerWidth, window.innerHeight );
 };
 
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.005;
-	cube.rotation.y += 0.005;
-
-	renderer.render( scene, camera );
-}
-
-animate();
